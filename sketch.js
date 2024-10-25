cb5d06f8b809cc31e43e8f62b7090087e118079a
joaogomes1909 <joao.gomes.rosa@escola.pr.gov.br>
2024-10-25 10:05:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() { // Criando o cenário
+  createCanvas(600, 600); //
+  background("white");
+ } 
+  
+function draw() {
+
+  stroke("blue");
+  fill("red");
+
+// console.log(mouseIspressed);
+
+ if(mouseIspressed) {// Quando o mouse for pressionado
+rect(mouseX, mouseY,20,35
+
+  
+  
